c1aaf66b19eb901b4baf01f78d4abe8fbb316ba2
David FletcherE <dtsfletcher@gmail.com>
2017-09-24 17:22:17 -0400

    JSON works

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,9 +5,10 @@ class BooksController < ApplicationController
     respond_to do |format|
       format.html
       format.text
+      format.json {render json: @books}
+      format.csv do |book|
+      book.generate_csv {render text: @books.to_csv}
 
-    format.csv do |book|
-    book.generate_csv {render text: @books.to_csv}
 
 
 
